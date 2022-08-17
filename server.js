0cada0dd60738ad5b99a318f29e4ba1bd81d3ab2
joseph muchene <ngugimuchene@gmail.com>
2022-08-17 17:40:58 +0300

    initial commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/api/v1/post", require("./routes/postRoute"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
